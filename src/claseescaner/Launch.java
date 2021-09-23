c6afb16f1e3c96e981cb926312ca99df0160933a
GamuFP <victordiez.ceep@gmail.com>
2021-09-23 09:58:26 +0200

    Commit inicial + gitignore

---
+++ src/claseescaner/Launch.java
@@ -0,0 +1,21 @@
+package claseescaner;
+
+import java.util.Scanner;
+
+
+public class Launch {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Bienvenido, ¿Cual es tu color favorito?");
+		
+		String color_favorito = scan.nextLine();
+		
+		System.out.println("Tu color favorito es el "+color_favorito);
+		
+		scan.close();
+
+	}
+
+}
