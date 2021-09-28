c55467a507646b98f3ab9c71cf08e2af3f95ec70
GamuCEEP <91058173+GamuCEEP@users.noreply.github.com>
2021-09-28 09:22:03 +0200

    Update Lacteos.java

--- src/tarea1cesar/Lacteos.java
+++ src/tarea1cesar/Lacteos.java
@@ -2,7 +2,7 @@ package tarea1cesar;
 
 public class Lacteos extends Market {
 
-	public byte num_proveedores, num_repartidores;
+	public byte num_proveedores, num_repartidores, num_empleadosL;
 
 	// Constructor basico
 	public Lacteos(byte num_empleados, byte num_proveedores) {
@@ -12,14 +12,14 @@ public class Lacteos extends Market {
 		// Inicializa los atributos del objeto
 		this.num_repartidores = (byte) (Math.random() * 3 + 3);
 		this.num_proveedores = num_proveedores;
-
-		// Añade el numero de empleados a la variable de market
+		this.num_empleadosL = num_empleados;
+		// AÃ±ade el numero de empleados a la variable de market
 		Market.num_empleados += num_empleados;
 	}
 
 	// Muestra toda la informacion del objeto (Sobrescritura)
 	public void mostrar() {
-		System.out.println("num_empleados=" + num_empleados + ", num_proveedores=" + num_proveedores
+		System.out.println("num_empleados=" + num_empleados + "num_empleadosL"+ num_empleadosL + ", num_proveedores=" + num_proveedores
 				+ ", num_repartidores=" + num_repartidores + ", ciudad=" + ciudad + ", direccion=" + direccion);
 
 	}
